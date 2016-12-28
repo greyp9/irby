c6b809d03483bba39dd3ef61c258d2e68151c57c
greyp9 <greyp9@yahoo.com>
2016-12-28 01:07:11 +0000

    utility to set process USERHOME

---
+++ src/main/app/java/io/github/greyp9/irby/app/exec/App.java
@@ -0,0 +1,26 @@
+package io.github.greyp9.irby.app.exec;
+
+import io.github.greyp9.arwo.core.lang.PlatformU;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class App {
+
+    private App() {
+    }
+
+    private int run() throws IOException, URISyntaxException {
+        final String[] commandArray = { "java", "-jar", "irby.jar" };
+        final File dir = PlatformU.getCodeFolder(getClass());
+        final Runtime runtime = Runtime.getRuntime();
+        /* final Process process = */
+        runtime.exec(commandArray, null, dir);
+        return 0;
+    }
+
+    public static void main(final String[] args) throws Exception {
+        System.exit(new App().run());
+    }
+}
