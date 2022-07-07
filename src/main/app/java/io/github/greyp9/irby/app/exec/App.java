3784bd80fdde928148ed3477c839c47d110547b3
greyp9 <greyp9@yahoo.com>
2022-07-07 15:33:41 -0400

    allow pass of command line args to daemon process

--- src/main/app/java/io/github/greyp9/irby/app/exec/App.java
+++ src/main/app/java/io/github/greyp9/irby/app/exec/App.java
@@ -5,23 +5,28 @@ import io.github.greyp9.arwo.core.lang.PlatformU;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public final class App {
 
     private App() {
     }
 
-    private int run() throws IOException, URISyntaxException {
+    private int run(final String[] args) throws IOException, URISyntaxException {
         final File fileJarD = PlatformU.getCodeLocation(getClass());
         final String filenameJar = fileJarD.getName().replace("-d", "");  // filename of primary jar
         final String classpath = filenameJar + ":lib/*";  // Linux, MacOS safe
-        final String[] commandArray = {
-                "java",
-                "-cp",
-                classpath,
-                "io.github.greyp9.irby.app.arwo.App"
-        };
-
+        // assemble exec command
+        final List<String> command = new ArrayList<>();
+        command.add("java");  // executable
+        command.addAll(Arrays.asList(args));  // input args (e.g. debugger)
+        command.add("-cp");  // class path
+        command.add(classpath);
+        command.add("io.github.greyp9.irby.app.arwo.App");  // startup class
+        final String[] commandArray = command.toArray(new String[0]);
+        System.out.println(Arrays.asList(commandArray));  // user feedback
         final File dir = PlatformU.getCodeFolder(getClass());
         final Runtime runtime = Runtime.getRuntime();
         /* final Process process = */
@@ -30,6 +35,6 @@ public final class App {
     }
 
     public static void main(final String[] args) throws Exception {
-        System.exit(new App().run());
+        System.exit(new App().run(args));
     }
 }
