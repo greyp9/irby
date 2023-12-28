b971b58826ac8fad342f50071940489dc36ae9a8
greyp9 <greyp9@yahoo.com>
2023-12-28 16:25:42 -0500

    refresh daemon script (current)

--- src/main/app/resources/sh/irby.sh
+++ src/main/app/resources/sh/irby.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
+IRBY_VERSION=0.2.0-SNAPSHOT
+IRBY_HOME=/home/irby/irby-$IRBY_VERSION
+
 # https://github.com/fhd/init-script-template/blob/master/template
 
 start()
 {
 echo "Starting..."
-daemon -nirby -uirby -D/home/irby -r -A1 -L10 -- /usr/bin/java -Djava.util.logging.config.file=logging.properties -jar irby.jar
+daemon -nirby -uirby -D$IRBY_HOME -r -A1 -L10 -- /usr/bin/java -Djava.util.logging.config.file=$IRBY_HOME/conf/logging.properties -jar irby-$IRBY_VERSION.jar
 }
 
 stop()
